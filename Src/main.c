1044c2d2bb036885fcda2144c53431320802f7b9
Brombeck Jean <156657798+BrombeckJean@users.noreply.github.com>
2024-10-10 10:53:21 +0200

    Add files via upload
    pb github création du répository

---
+++ Src/main.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <stm32f446xx.h>
+#include <stdio.h>
+#include "main.h"
+#include "gpio.h"
+#include "usart.h"
+#include "timer.h"
+
+
+int main(void)
+{
+	GPIO_Init();
+	USART2_Init();
+	SYSTICK_Init();
+
+	while(1){
+		//for(uint32_t i = 0 ; i<100000; i++);
+		SYSTICK_Delay(1000);
+		GPIOA->ODR ^= 1<<5;
+		printf("Hello\r\n");
+	}
+}
